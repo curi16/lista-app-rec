8027b9cc28ac8d08788de470d3e12c1ad8ef75a8
curi16 <luisfelipecuri10@gmail.com>
2025-11-11 10:44:45 -0300

    5.a

---
+++ 5/a.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Contador() {
+  const [count, setcount] = useState(0);
+
+  return (
+    <div>
+      <h2>Contador: {count}</h2>
+      <button onClick={() => setcount(count + 1)}>+1</button>
+      <button onClick={() => setcount(count - 1)}>-1</button>
+    </div>
+  );
+}
+
+export default Contador
