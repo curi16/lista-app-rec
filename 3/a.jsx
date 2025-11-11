4cddf63d25fec4fdce9e068831410ce7c90bc3df
curi16 <luisfelipecuri10@gmail.com>
2025-11-11 10:38:00 -0300

    3.a

---
+++ 3/a.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import React from 'react';
+
+function ListaHobbies() {
+  const hobbies = ['Leitura', 'Natação', 'Programação', 'Culinária'];
+  return (
+    <div>
+      <h2>Meus Hobbies Favoritos</h2>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ListaHobbies;
