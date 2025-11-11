2d4d95ade29a2aa6456a7d41f45204fc9d09cc57
curi16 <luisfelipecuri10@gmail.com>
2025-11-11 10:29:17 -0300

    2.a

---
+++ 2/a.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import React from 'react';
+
+function Saudacao({ nome }) {
+  return <h1>Olá, {nome}!</h1>;
+}
+
+export default function MyApp() {
+  return (<Saudacao nome="Felipe" />)
+}
